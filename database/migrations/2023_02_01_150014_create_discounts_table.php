ff85f55c7b586b4d7869bb6d3f3afd833796d390
Tim Venetz <tim.venetz@lernende.bfo-vs.ch>
2023-02-01 16:29:19 +0100

    feature/8-create-database-migrations

---
+++ database/migrations/2023_02_01_150014_create_discounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discounts', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('discount_price',10,2);
+            $table->date('discount_from');
+            $table->foreignId('product_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discounts');
+    }
+};
