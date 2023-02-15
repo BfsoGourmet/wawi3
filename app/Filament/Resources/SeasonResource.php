68d5f6710fe3760a7c4f29ef736225f8f28aa830
Venetz Robin <venetz.robin@lernende.bfo-vs.ch>
2023-02-15 14:46:11 +0100

    label fixes

--- app/Filament/Resources/SeasonResource.php
+++ app/Filament/Resources/SeasonResource.php
@@ -3,10 +3,8 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\SeasonResource\Pages;
-use App\Filament\Resources\SeasonResource\RelationManagers;
 use App\Models\Season;
 use Filament\Forms;
-use Filament\Forms\Components\TextInput;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
@@ -18,19 +16,32 @@ class SeasonResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-collection';
 
+    protected static ?string $modelLabel = 'Season';
+
+    protected static ?string $modelPluralLabel = 'Season';
+
+    protected static ?string $navigationLabel = 'Seasons';
+
+    protected static ?string $navigationGroup = 'Product components';
+
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
                 Forms\Components\TextInput::make('name')
+                    ->label('Season')
                     ->required(),
 
                 Forms\Components\Repeater::make('seaonDates')
+                    ->label('Season dates')
                     ->relationship('seasonDates')
+                    ->createItemButtonLabel('Add season date')
                     ->schema([
                         Forms\Components\DateTimePicker::make('date_from')
+                            ->label('From')
                             ->required(),
                         Forms\Components\DateTimePicker::make('date_until')
+                            ->label('Until')
                             ->after(function ($get) { return $get('date_from'); })
                             ->required()
                     ])
@@ -42,6 +53,7 @@ class SeasonResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('name')
+                    ->label('Season')
             ])
             ->filters([
                 //
